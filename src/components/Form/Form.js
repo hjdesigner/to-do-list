b04a1b66c13e4e20fa2139f9a60eaebdb34e6de4
Henrique Melanda <hjdesigner23@gmail.com>
2020-03-13 15:24:29 -0300

    - Done with the API to create a new task; - Created the function to create a tag and delete a tag; - Validation done to only enable the save button if you fill in the title, date and time; - Added the toastify lib to give feedback in case of success or error in the API call; - Added a lib axios to make requests;

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import { v4 as uuidv4 } from 'uuid';
+import axios from 'axios';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
+import { Link } from 'react-router-dom';
 import { Input } from 'components/Input';
 import { Select } from 'components/Select';
 import { TextArea } from 'components/TextArea';
 import { Button } from 'components/Button';
+import { HOME } from 'utils/routes'
 
 const options = [
   {
@@ -23,31 +29,134 @@ const options = [
   },
 ];
 
-const Form = () => (
-  <Element>
-    <Input name='titulo' title='Title' />
-    <ElementMultiply>
-      <DateOfTheEvent>
-        <Input name='data' title='Data do evento' />
-      </DateOfTheEvent>
-      <TimeOfTheEvent>
-        <Input name='hora' title='Hora do evento' />
-      </TimeOfTheEvent>
-      <RememberMe>
-        <Select name='lembre-me' title='Lembre-me' options={options} />
-      </RememberMe>
-    </ElementMultiply>
-    <Tags>
-      <Input name='tag' title='Tag' />
-      <AddTag>Adicionar</AddTag>
-    </Tags>
-    <TextArea name='descricao' title='Descrição' />
-    <Actions>
-      <Add>Salvar</Add>
-      <Cancel>Cancelar</Cancel>
-    </Actions>
-  </Element>
-);
+const Form = () => {
+  const [title, setTitle] = useState('');
+  const [date, setDate] = useState('');
+  const [time, setTime] = useState('');
+  const [remembeMe, setRemembeMe] = useState(5);
+  const [tag, setTag] = useState('');
+  const [tags, setTags] = useState([]);
+  const [description, setDescription] = useState('');
+
+  const handleDate = (value) => {
+    if (value.length <= 10) {
+      setDate(value.replace(/[^\d/]/g, ""));
+    }
+  }
+  const handleTime = (value) => {
+    if (value.length <= 5) {
+      setTime(value.replace(/[^\d:]/g, ""));
+    }
+  }
+  const handleAddTag = () => {
+    setTags([
+      ...tags,
+      {
+        id: uuidv4(),
+        name: tag,
+      }
+    ]);
+    setTag('');
+  }
+  const handleDeleteTag = (id) => {
+    const newTags = tags.filter(item => item.id !== id);
+    setTags(newTags);
+  }
+  const handleSave = () => {
+    const data = {
+      title,
+      date,
+      time,
+      reminder: remembeMe,
+      tags,
+      description,
+    }
+
+    axios.post('http://5e6b9daed708a000160b4bbc.mockapi.io/api/v1/task', data)
+      .then((response) => {
+        setTitle('');
+        setDate('');
+        setTime('');
+        setRemembeMe(5);
+        setTags([]);
+        setDescription('');
+        toast.success('Tarefa criada com sucesso');
+      })
+      .catch((error) => {
+        toast.error('Houve um erro, tente novamente');
+      });
+  }
+
+  return (
+    <Element>
+      <Input name='titulo' title='Title' value={title} onChange={e => setTitle(e.target.value)} />
+      <ElementMultiply>
+        <DateOfTheEvent>
+          <Input
+            name='data'
+            title='Data do evento'
+            value={date}
+            onChange={e => handleDate(e.target.value)}
+            placeholder='20/12/2020'
+          />
+        </DateOfTheEvent>
+        <TimeOfTheEvent>
+          <Input
+            name='hora'
+            title='Hora do evento'
+            value={time}
+            onChange={e => handleTime(e.target.value)}
+            placeholder='04:20'
+          />
+        </TimeOfTheEvent>
+        <RememberMe>
+          <Select
+            name='lembre-me'
+            title='Lembre-me'
+            options={options}
+            value={remembeMe}
+            onChange={e => setRemembeMe(e.target.value)}
+          />
+        </RememberMe>
+      </ElementMultiply>
+      <Tags>
+        <Input
+          name='tag'
+          title='Tag'
+          value={tag}
+          onChange={e => setTag(e.target.value)}
+        />
+        <AddTag onClick={handleAddTag}>Adicionar</AddTag>
+      </Tags>
+      <TagsValues>
+        {tags.map((tag) => (
+          <Tag key={tag.id}>
+            {tag.name}
+            <DeleteTag onClick={() => handleDeleteTag(tag.id)}>X</DeleteTag>
+          </Tag>
+        ))}
+      </TagsValues>
+      <TextArea
+        name='descricao'
+        title='Descrição'
+        value={description}
+        onChange={e => setDescription(e.target.value)}
+      />
+      <Actions>
+        <Add
+          disabled={
+            title === '' ||
+            date === '' ||
+            time === ''
+          }
+          onClick={() => handleSave()}
+        >Salvar</Add>
+        <Cancel to={HOME}>Cancelar</Cancel>
+      </Actions>
+      <ToastContainer />
+    </Element>
+  );
+}
 
 const Element = styled.div``;
 const ElementMultiply = styled.div`
@@ -58,7 +167,7 @@ const ElementMultiply = styled.div`
 `;
 const DateOfTheEvent = styled.div`
   margin-bottom calc(${({ theme }) => theme.spaces}px * 2);
-  max-width: 100%;
+  width: 100%;
 
   @media screen and (min-width: ${props => props.theme.media.tablet}) {
     max-width: 200px;
@@ -87,20 +196,46 @@ const Add = styled(Button)`
   font-size: 1.4em;
   padding: ${({ theme }) => theme.spaces}px;
   width: auto;
+
+  &[disabled] {
+    cursor: default;
+    opacity: .5;
+  }
 `;
-const Cancel = styled(Add)`
+const Cancel = styled(Link)`
   background-color: ${({ theme }) => theme.colors.red};
+  color: ${({ theme }) => theme.colors.white};
+  font-size: 1.4em;
+  padding: ${({ theme }) => theme.spaces}px;
   margin-left: ${({ theme }) => theme.spaces}px;
+  text-decoration: none;
 `;
 const Tags = styled.div`
   align-items: flex-end;
   display: flex;
-  margin-bottom: calc(${({ theme }) => theme.spaces}px * 3);  
 `;
 const AddTag = styled(Add)`
   height: 30px;
   padding: 0 ${({ theme }) => theme.spaces}px;
   margin-left: calc(${({ theme }) => theme.spaces}px * 2);
+`;
+const TagsValues = styled.div`
+  display: flex;
+  margin-bottom: calc(${({ theme }) => theme.spaces}px * 3);
+`;
+const Tag = styled.p`
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.gray};
+  display: flex;
+  font-size: 1.3em;
+  justify-content: center;
+  padding: ${({ theme }) => theme.spaces}px;
+  margin-top: calc(${({ theme }) => theme.spaces}px * 2);
+  margin-right: calc(${({ theme }) => theme.spaces}px * 2);
+  margin-bottom: 0;
+`;
+const DeleteTag = styled(Button)`
+  margin-left: ${({ theme }) => theme.spaces}px;
 `;
 
 export default Form;
