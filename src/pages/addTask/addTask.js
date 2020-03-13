522634f3b79410cc8abb51baa576e75bfe45d815
Henrique Melanda <hjdesigner23@gmail.com>
2020-03-13 11:47:57 -0300

    Styled title page add task

--- src/pages/addTask/addTask.js
+++ src/pages/addTask/addTask.js
@@ -4,7 +4,7 @@ import { Form } from 'components/Form';
 
 const AddTask = () => (
   <Container>
-    <h1>Adicionar Tarefa</h1>
+    <Title>Adicionar Tarefa</Title>
 
     <FormElement>
       <Form />
@@ -17,12 +17,16 @@ const Container = styled.main`
   margin: calc(${props => `${props.theme.spaces}px`} * 2) auto 0;
   max-width: 1024px;
 `;
-
 const FormElement = styled.section`
   background-color: ${({ theme }) => theme.colors.white};
   list-style: none;
   padding: calc(${({ theme }) => theme.spaces}px * 2);
   margin-top: calc(${({ theme }) => theme.spaces}px * 2);
+`;
+const Title = styled.h2`
+  color: ${({ theme }) => theme.colors.blue};
+  font-size: 1.8em;
+  margin: calc(${props => `${props.theme.spaces}px`} * 3) 0;
 `;
 
 export default AddTask;
