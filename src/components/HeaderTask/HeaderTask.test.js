a8092d3a59cfd3ed1e948d9ee9c781ae69949d71
Henrique Melanda <hjdesigner23@gmail.com>
2020-03-13 17:19:52 -0300

    Fixing the tests of the LeaderTask component which is now not a button but a link

--- src/components/HeaderTask/HeaderTask.test.js
+++ src/components/HeaderTask/HeaderTask.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Enzyme, { mount } from 'enzyme';
+import { BrowserRouter } from 'react-router-dom';
 import 'jest-styled-components';
 import Adapter from 'enzyme-adapter-react-16';
 Enzyme.configure({ adapter: new Adapter() });
@@ -8,17 +9,17 @@ import HeaderTask from './HeaderTask';
 
 describe('Style component <HeaderTask />', () => {
   
-  test('You should rennder the button with the color #4e7cfd', () => {
-    const wrapper = mount(<Theme><HeaderTask /></Theme>);
-    expect(wrapper.find('Button')).toHaveStyleRule('color', '#4e7cfd');
+  test('You should rennder the Link with the color #4e7cfd', () => {
+    const wrapper = mount(<BrowserRouter><Theme><HeaderTask /></Theme></BrowserRouter>);
+    expect(wrapper.find('Link')).toHaveStyleRule('color', '#4e7cfd');
   });
-  test('You should rennder the button with the padding calc(8px * 2) 0', () => {
-    const wrapper = mount(<Theme><HeaderTask /></Theme>);
-    expect(wrapper.find('Button')).toHaveStyleRule('padding', 'calc(8px * 2) 0');
+  test('You should rennder the Link with the padding calc(8px * 2) 0', () => {
+    const wrapper = mount(<BrowserRouter><Theme><HeaderTask /></Theme></BrowserRouter>);
+    expect(wrapper.find('Link')).toHaveStyleRule('padding', 'calc(8px * 2) 0');
   }); 
   test('Should return the padding with the value 8px calc(8px * 2) qhen the resolution is 768px', () => {
-    const wrapper = mount(<Theme><HeaderTask /></Theme>);
-    expect(wrapper.find('Button')).toHaveStyleRule('padding', '8px calc(8px * 2)', {
+    const wrapper = mount(<BrowserRouter><Theme><HeaderTask /></Theme></BrowserRouter>);
+    expect(wrapper.find('Link')).toHaveStyleRule('padding', '8px calc(8px * 2)', {
       media: `screen and (min-width: 768px)`
     })
   }); 
